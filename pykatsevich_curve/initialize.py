bbed5374a414b0ae757ec501a32cd50ab708f7de
zy7_newserver <569915578@163.com>
2025-06-24 17:21:43 +0200

    1

---
+++ pykatsevich_curve/initialize.py
@@ -0,0 +1,213 @@
+# -----------------------------------------------------------------------
+# This file is part of Pykatsevich distribution (https://github.com/astra-toolbox/helical-kats).
+# Copyright (c) 2024 imec-Vision Lab, University of Antwerp.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------
+
+import numpy as np
+
+def create_configuration(
+    scan_geometry : dict,
+    astra_volume_geometry : dict,
+    katsevich_options: dict={}
+):
+    """
+    Create the helical CT configuration from the corresponding projection and volume geometries.
+
+    Parameters:
+    ===========
+    scan_geometry : dict
+        Dictionary with the following keys:["SOD", "SDD", "detector", "helix"].
+        Keys in "detector": ["detector psize", "detector rows", "detector cols"].
+        Keys in "helix": ["angles_count", "pitch_mm_rad"].
+        Optional key in "helix": "angles_range".
+    astra_volume_geometry : dict
+        Dictionary with the ASTRA volume geometry. See astra.create_vol_geom(...) for details.
+
+    katsevich_options: dict
+        Dictionary with options for Katsevich's filtering.
+        Supported keys: ["detector_rebin_rows"]
+    Returns:
+    ========
+    helical_conf : dict
+        Helical configuration.
+    """
+
+    # angles_helical = np.linspace(
+    #     scan_geometry["angle_start"],
+    #     scan_geometry["angle_end"],
+    #     scan_geometry["angles_count"],
+    #     endpoint=False
+    # )
+    # ang_step = angles_helical[1]-angles_helical[0]
+    # magnification = scan_geometry["SDD"] / scan_geometry["SOD"]
+    # h_stride = ang_step*(scan_geometry["pitch"] * scan_geometry["detector psize"] * scan_geometry["detector rows"] / (2*np.pi)) / magnification
+    
+    # Configure specific geometry parameters useful for Katsevish filter:
+    helical_conf = {}
+
+    helical_conf['total_projs'] = scan_geometry['helix']['angles_count']
+
+    helical_conf['x_max'] = astra_volume_geometry['option']['WindowMaxX']
+    helical_conf['x_min'] = astra_volume_geometry['option']['WindowMinX']
+    helical_conf['y_max'] = astra_volume_geometry['option']['WindowMaxY']
+    helical_conf['y_min'] = astra_volume_geometry['option']['WindowMinY']
+    helical_conf['z_max'] = astra_volume_geometry['option']['WindowMaxZ']
+    helical_conf['z_min'] = astra_volume_geometry['option']['WindowMinZ']
+    # print(astra_volume_geometry)
+    helical_conf['x_voxels'] = astra_volume_geometry['GridColCount']
+    helical_conf['y_voxels'] = astra_volume_geometry['GridRowCount']
+    helical_conf['z_voxels'] = astra_volume_geometry['GridSliceCount']
+
+    helical_conf['x_len'] = np.float32(helical_conf['x_max'] - helical_conf['x_min'])
+    helical_conf['y_len'] = np.float32(helical_conf['y_max'] - helical_conf['y_min'])
+    helical_conf['z_len'] = np.float32(helical_conf['z_max'] - helical_conf['z_min'])
+    helical_conf['delta_x'] = np.float32(helical_conf['x_len'] / (helical_conf['x_voxels'])) # np.float32(helical_conf['x_len'] / (helical_conf['x_voxels'] - 1))
+    helical_conf['delta_y'] = np.float32(helical_conf['y_len'] / (helical_conf['y_voxels'])) # np.float32(helical_conf['y_len'] / (helical_conf['y_voxels'] - 1))
+    helical_conf['delta_z'] = np.float32(helical_conf['z_len'] / max(helical_conf['z_voxels'], 1 )) # np.float32(helical_conf['z_len'] / max(helical_conf['z_voxels'] - 1, 1))
+
+    helical_conf['detector rows'] = scan_geometry['detector']['detector rows']
+    helical_conf['detector cols'] = scan_geometry['detector']['detector cols']
+
+    # helical_conf['progress_per_radian'] = h_stride  / ang_step # mm per radian
+    helical_conf['progress_per_radian'] = scan_geometry['helix']['pitch_mm_rad']
+    helical_conf['progress_per_turn']   = helical_conf['progress_per_radian'] * 2*np.pi
+
+    helical_conf['scan_diameter'] = scan_geometry["SDD"] # scan_geometry["SOD"] + scan_geometry["SDD"]
+    helical_conf['scan_radius'] = scan_geometry["SOD"] # Used to be (which is incorrect): 0.5 * helical_conf['scan_diameter']
+    
+    if "angles_range" in scan_geometry['helix'].keys():
+        helical_conf['s_len'] = scan_geometry['helix']['angles_range']
+        helical_conf['s_min'] = -helical_conf['s_len'] * 0.5
+        helical_conf['s_max'] =  helical_conf['s_len'] * 0.5
+    else:
+        helical_conf['s_min'] = -np.pi + helical_conf['z_min'] / helical_conf['progress_per_radian']
+        helical_conf['s_max'] =  np.pi + helical_conf['z_max'] / helical_conf['progress_per_radian']
+        helical_conf['s_len'] = helical_conf['s_max'] - helical_conf['s_min']
+
+    helical_conf['projs_per_turn'] = helical_conf['total_projs'] / helical_conf['s_len'] * 2*np.pi
+
+    helical_conf['delta_s'] = 2*np.pi / helical_conf['projs_per_turn'] # Turn in radians per one projection
+
+    # print(f"projs_per_turn = {helical_conf['projs_per_turn']},\
+    #         progress_per_radian = {helical_conf['progress_per_radian']}, progress_per_turn={helical_conf['progress_per_turn']}, delta_s={helical_conf['delta_s']},\
+    #         s_len={helical_conf['s_len']}")
+
+    helical_conf['source_pos'] = helical_conf['s_min'] + helical_conf['delta_s'] * (np.arange(helical_conf['total_projs'], dtype=np.float32) + 0.5 )
+
+    # print(f"s_min = {helical_conf['s_min']}, s_max = {helical_conf['s_max']}")
+    # print(f"helical_conf['source_pos'] = {helical_conf['source_pos']}")
+
+    helical_conf['detector_pixel_span_u'] = scan_geometry['detector']['detector psize u']
+    helical_conf['detector_pixel_span_v'] = scan_geometry['detector']['detector psize v']
+
+    helical_conf['detector_pixel_width'] = helical_conf['detector_pixel_span_u']
+    helical_conf['detector_pixel_height'] = helical_conf['detector_pixel_span_v']
+
+    helical_conf['pixel_span'] = helical_conf['detector_pixel_span_u']
+    helical_conf['pixel_height'] = helical_conf['detector_pixel_span_v']
+
+    helical_conf['M'] = scan_geometry['condition']['M']
+    helical_conf['detector_rebin_rows'] = helical_conf['M'] # The default value is 64
+    # print(f"detector_rebin_rows = {helical_conf['detector_rebin_rows']}")
+
+    helical_conf['fov_diameter'] = max(helical_conf['x_len'], helical_conf['y_len'])
+    helical_conf['fov_radius'] = 0.5 * helical_conf['fov_diameter']
+    helical_conf['half_fan_angle'] = np.arcsin(helical_conf['fov_radius'] / helical_conf['scan_radius'])
+    
+    helical_conf['detector_rebin_rows_height'] = (np.pi + 2* helical_conf['half_fan_angle']) / (helical_conf['detector_rebin_rows'] - 1)
+
+    helical_conf['col_coords'] = scan_geometry['detector']['detector psize u'] * (np.arange(scan_geometry['detector']['detector cols'] + 1, dtype=np.float32)
+              + 0.0 # Here "0.0" is a hardcoded value of conf['detector_column_offset']
+              - 0.5*(scan_geometry['detector']['detector cols'] - 1)
+              ) # EXTENDED coordinates!
+
+    helical_conf['row_coords'] = scan_geometry['detector']['detector psize v'] * (np.arange(scan_geometry['detector']['detector rows'] + 1, dtype=np.float32)
+              + 0.0 # Here "0.0" is a hardcoded value of conf['detector_column_offset']
+              - 0.5*(scan_geometry['detector']['detector rows'] - 1)
+              ) # EXTENDED coordinates!
+
+    rebin_coords =  -np.pi / 2 - helical_conf['half_fan_angle'] + helical_conf['detector_rebin_rows_height'] * np.arange(helical_conf['detector_rebin_rows'], dtype=np.float32)
+
+    rebin_scale = ( helical_conf['scan_diameter'] * helical_conf['progress_per_turn']) / (2 * np.pi * helical_conf['scan_radius'] )
+
+    # Simplified version of original scale expression:
+    # rebin_scale = 2 * helical_conf['progress_per_radian']
+
+    # skip last column from differentiation
+    col_coords = helical_conf['col_coords'][:-1]
+
+    helical_conf['fwd_rebin_row'] = np.zeros( (helical_conf['detector_rebin_rows'], helical_conf['detector cols']) )
+    for col in range(helical_conf['detector cols']):
+        row = rebin_scale * (rebin_coords + rebin_coords
+                            / np.tan(rebin_coords) * (col_coords[col]
+                            / helical_conf['scan_diameter']))
+        helical_conf['fwd_rebin_row'][:, col] = row
+
+    # Hilbert filter:
+    helical_conf['kernel_radius'] = helical_conf['detector cols'] - 1
+    helical_conf['kernel_width'] = 1 + 2 * helical_conf['kernel_radius']
+    helical_conf['proj_filter_width'] = helical_conf['detector cols']
+
+    # Tam-Danialsson boundaries, w_top and w_bottom, from Noo et al., Eq. (78):
+    proj_row_maxs = -helical_conf['progress_per_turn'] / (2*np.pi*helical_conf["scan_radius"]*helical_conf["scan_diameter"]) * (helical_conf['col_coords']**2 + helical_conf['scan_diameter']**2) * (np.pi/2 + np.arctan(helical_conf['col_coords']/helical_conf['scan_diameter']))
+    proj_row_mins =  helical_conf['progress_per_turn'] / (2*np.pi*helical_conf["scan_radius"]*helical_conf["scan_diameter"]) * (helical_conf['col_coords']**2 + helical_conf['scan_diameter']**2) * (np.pi/2 - np.arctan(helical_conf['col_coords']/helical_conf['scan_diameter']))
+    
+    expandingfactor = 1
+    proj_row_mins = expandingfactor * proj_row_mins
+    proj_row_maxs = expandingfactor * proj_row_maxs
+
+    helical_conf['proj_row_mins'] = proj_row_mins
+    helical_conf['proj_row_maxs'] = proj_row_maxs
+
+    helical_conf['T-D smoothing'] = 0.6
+
+    rebin_row = np.zeros(shape=(helical_conf['detector rows'], helical_conf['detector cols']), dtype=np.int32)
+    
+    pos_start = int(0.5 * helical_conf['detector cols'])
+    
+    # Prepare helper arrays for reverse height rebinning:
+    fracs_0 = np.zeros(shape=(helical_conf['detector rows'], helical_conf['detector cols']))
+    fracs_1 = np.ones (shape=(helical_conf['detector rows'], helical_conf['detector cols']))
+    
+    for row in range(helical_conf['detector rows']):
+
+        for col in range(pos_start, helical_conf['detector cols']):
+            rebin_row[row, col] = 0
+            for rebin in range(helical_conf["detector_rebin_rows"] - 1):
+                if (helical_conf['row_coords'][row] >= helical_conf['fwd_rebin_row'][rebin, col]) \
+                    and (helical_conf['row_coords'][row] <= helical_conf['fwd_rebin_row'][rebin + 1, col]):
+                    rebin_row[row, col] = rebin
+                    break
+
+            fracs_0[row, col] = (helical_conf['row_coords'][row] - helical_conf['fwd_rebin_row'][rebin_row[row, col], col]) \
+                / (helical_conf['fwd_rebin_row'][rebin_row[row, col] + 1, col] - helical_conf['fwd_rebin_row'][rebin_row[row, col], col])
+
+        for col in range(pos_start):
+            rebin_row[row, col] = 1
+            for rebin in range(helical_conf['detector_rebin_rows'] - 1, 0, -1):
+                if (helical_conf['row_coords'][row] >= helical_conf['fwd_rebin_row'][rebin - 1, col]) \
+                    and (helical_conf['row_coords'][row] <= helical_conf['fwd_rebin_row'][rebin, col]):
+                        rebin_row[row, col] = rebin
+                        break
+            fracs_0[row, col] = (helical_conf['row_coords'][row] - helical_conf['fwd_rebin_row'][rebin_row[row, col] - 1, col]) \
+                / (helical_conf['fwd_rebin_row'][rebin_row[row, col], col] - helical_conf['fwd_rebin_row'][rebin_row[row, col] - 1, col])
+
+    fracs_1 -= fracs_0
+    
+    helical_conf['rebin_row'] = rebin_row
+    helical_conf['rebin_fracs_0'] = fracs_0
+    helical_conf['rebin_fracs_1'] = fracs_1
+
+    return helical_conf
---
+++ tests/Naeotom_curve.py
@@ -0,0 +1,242 @@
+# -----------------------------------------------------------------------
+# This file is part of Pykatsevich distribution (https://github.com/astra-toolbox/helical-kats).
+# Copyright (c) 2024 imec-Vision Lab, University of Antwerp.
+# 
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# This program is distributed in the hope that it will be useful, but 
+# WITHOUT ANY WARRANTY; without even the implied warranty of 
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+# -----------------------------------------------------------------------
+
+"""
+This test runs ASTRA's backprojection on filtered sinogram.
+"""
+def test_pipeline(settings_file):
+
+    from tests.common import phantom_objects_3d, project, animate_volume, backproject
+
+    import numpy as np
+    from matplotlib import pyplot as plt
+    from time import time
+    import yaml
+    import os
+    import astra
+    import sys
+    import tifffile
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    from pykatsevich_curve.initialize import create_configuration
+
+    try:
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+    except:
+        print("Failed to get __file__, using current working directory instead")
+        test_dir = os.getcwd()
+
+    # 拼接 yaml 文件完整路径
+    yaml_path = os.path.join(test_dir, settings_file)
+
+    if not os.path.exists(yaml_path):
+        raise FileNotFoundError(f"配置文件未找到: {yaml_path}")
+
+    with open(yaml_path, "r") as file:
+        yaml_settings = yaml.safe_load(file)
+
+    recon = yaml_settings['recon']
+    recon_shape = (recon['rows'], recon['columns'], recon['slices']) 
+    voxel_size = recon['voxel_size']
+    geom = yaml_settings['geometry']
+    
+
+
+    sinogram = tifffile.imread(r"E:\Projects\Liu_proj\pykats\pyKatsevich\scan_001_flat_helix_projections.tif")
+    vol_geom, proj_geom,_ = generate_astra_geom(recon_shape, voxel_size, geom)
+
+    conf=create_configuration(
+        geom,
+        vol_geom,
+        yaml_settings['geometry'].get('options', {})
+    )
+
+
+    from pykatsevich.reconstruct import reconstruct
+
+    rec_astra = reconstruct(
+        sinogram,
+        conf,
+        vol_geom,
+        proj_geom,
+        {
+            "Diff": {"Progress bar": True, "Print time": True},
+            "FwdRebin": {"Progress bar": True, "Print time": True},
+            "BackRebin": {"Progress bar": True, "Print time": True},
+            "BackProj": {"Progress bar": True, "Print time": True},
+        }
+    )
+
+    print('done')
+    tifffile.imwrite('recon.tif',rec_astra)
+
+def only_reconstruct_pipeline(settings_file):
+    import yaml
+    import os
+    import astra
+    import sys
+    import tifffile
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+    from pykatsevich.initialize import create_configuration
+
+    try:
+        test_dir = os.path.dirname(os.path.abspath(__file__))
+    except:
+        print("Failed to get __file__, using current working directory instead")
+        test_dir = os.getcwd()
+
+    # 拼接 yaml 文件完整路径
+    yaml_path = os.path.join(test_dir, settings_file)
+
+    if not os.path.exists(yaml_path):
+        raise FileNotFoundError(f"配置文件未找到: {yaml_path}")
+
+    with open(yaml_path, "r") as file:
+        yaml_settings = yaml.safe_load(file)
+
+    recon = yaml_settings['recon']
+    recon_shape = (recon['rows'], recon['columns'], recon['slices']) 
+    voxel_size = recon['voxel_size']
+    geom = yaml_settings['geometry']
+    
+    filtered_sinogram = tifffile.imread(r"filtered_proj.tif")
+    vol_geom, proj_geom, lambda_list = generate_astra_geom(recon_shape, voxel_size, geom)
+
+    conf=create_configuration(
+        geom,
+        vol_geom,
+        yaml_settings['geometry'].get('options', {})
+    )
+
+    from pykatsevich.backproject_gpu import katsevich_backprojection_gpu
+    from pykatsevich.backproject import katsevich_backprojection_cpu
+    import numpy as np
+    # Step 1: 取中间部分投影
+    start_idx = 4000
+    end_idx = 7000
+    filtered_subset = filtered_sinogram[start_idx:end_idx]
+    lambda_subset = lambda_list[start_idx:end_idx]
+
+    # Step 2: 计算对应的 z 范围
+    P = conf['progress_per_turn']
+    z_start = conf['z_min'] + P * (lambda_subset[0] - lambda_list[0]) / (2 * np.pi) #+ P/2
+    z_end   = conf['z_min'] + P * (lambda_subset[-1] - lambda_list[0]) / (2 * np.pi) #- P/2
+    print('pitch, z_start, z_end', P, z_start, z_end)
+    z_min = z_start + P/2
+    z_max= z_end - P/2
+
+    conf['z_min'] = z_min
+    
+    # Step 3: 找出对应体素 z index 范围
+    z_vals = conf['z_min'] + np.arange(conf['z_voxels']) * conf['delta_z']
+    valid_z = np.where((z_vals >= z_min) & (z_vals <= z_max))[0]
+    recon_shape_subset = (conf['x_voxels'], conf['y_voxels'], len(valid_z))
+
+    print("total labmda start:", lambda_list[0])
+    print("min max z voxels: ", min(conf['z_voxels'], 1), max(conf['z_voxels'], 1))
+    print("min max z vals: ", min(z_vals), max(z_vals))
+    print("reconstruction slices number: ", len(valid_z))
+    print("reconstruction range z: ", z_min, z_max)
+    print("reconstruction range lambda: ", lambda_subset[0], lambda_subset[-1])
+    # Step 4: 重建
+    '''rec_partial = katsevich_backprojection_gpu(
+        filtered_subset,
+        conf,
+        recon_shape_subset,
+        lambda_subset
+    )'''
+
+    rec_partial = katsevich_backprojection_cpu(
+        filtered_subset,
+        conf,
+        recon_shape_subset,
+        lambda_subset
+    )
+
+    print('done')
+    tifffile.imwrite('recon.tif', rec_partial)
+
+def generate_astra_geom(volume_shape, voxel_size, helical_scan_geom):
+    """
+    Generate ASTRA vol_geom and proj_geom from volume shape and helical geometry settings.
+
+    Parameters:
+    ===========
+    volume_shape : tuple
+        Shape of the volume (rows - Y, cols - X, slices - Z)
+    voxel_size : float
+        Voxel size in mm
+    helical_scan_geom : dict
+        Dict with keys: ["SOD", "SDD", "detector", "helix"]
+
+    Returns:
+    ========
+    astra_vol_geom : dict
+        ASTRA volume geometry
+    astra_proj_geom : dict
+        ASTRA projection geometry (cone_vec)
+    """
+    import numpy as np
+    import astra
+    from pykatsevich.geometry import astra_helical_views_uv
+
+    rows, cols, slices = volume_shape
+
+    astra_vol_geom = astra.create_vol_geom(
+        rows, cols, slices,
+        -cols * voxel_size * 0.5, cols * voxel_size * 0.5,
+        -rows * voxel_size * 0.5, rows * voxel_size * 0.5,
+        -slices * voxel_size * 0.5, slices * voxel_size * 0.5
+    )
+
+    if "angles_range" in helical_scan_geom['helix']:
+        s_len = helical_scan_geom['helix']['angles_range']
+        s_min = -s_len * 0.5
+        s_max =  s_len * 0.5
+    else:
+        s_min = -np.pi + astra_vol_geom['option']['WindowMinZ'] / helical_scan_geom['helix']['pitch_mm_rad']
+        s_max =  np.pi + astra_vol_geom['option']['WindowMaxZ'] / helical_scan_geom['helix']['pitch_mm_rad']
+        s_len = s_max - s_min
+
+    projs_per_turn = helical_scan_geom['helix']["angles_count"] / (s_len / (2*np.pi))
+    delta_s = 2 * np.pi / projs_per_turn
+    angles = s_min + delta_s * (np.arange(helical_scan_geom['helix']["angles_count"], dtype=np.float32) + 0.5)
+    stride_mm = helical_scan_geom['helix']['pitch_mm_rad'] * delta_s
+    
+    views = astra_helical_views_uv(
+        helical_scan_geom["SOD"],
+        helical_scan_geom["SDD"],
+        helical_scan_geom['detector']["detector psize u"],
+        helical_scan_geom['detector']["detector psize v"],
+        angles,
+        stride_mm
+    )
+
+    astra_proj_geom = astra.create_proj_geom(
+        "cone_vec",
+        helical_scan_geom['detector']["detector rows"],
+        helical_scan_geom['detector']["detector cols"],
+        views
+    )
+
+    return astra_vol_geom, astra_proj_geom, angles
+
+
+if __name__=="__main__":
+    yaml_settings_file = "Naeotom_spine_10919.yaml"
+    test_pipeline(yaml_settings_file)
+
+
